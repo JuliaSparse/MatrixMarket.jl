c718007f6ff72855e642d6e6c5287d03e8e5e2f7
Tristan Konolige <tristan.konolige@gmail.com>
2016-09-29 21:36:24 -0600

    use do notation with open

--- src/MatrixMarket.jl
+++ src/MatrixMarket.jl
@@ -17,85 +17,86 @@ structure is returned from reading the header. The actual data for the
 matrix elements are not parsed.
 """
 function mmread(filename, infoonly::Bool=false)
-    mmfile = open(filename,"r")
-    # Read first line
-    firstline = chomp(readline(mmfile))
-    tokens = split(firstline)
-    if length(tokens) != 5
-        throw(ParseError(string("Not enough words on first line: ", ll)))
-    end
-    if tokens[1] != "%%MatrixMarket"
-        throw(ParseError(string("Not a valid MatrixMarket header:", ll)))
-    end
-    (head1, rep, field, symm) = map(lowercase, tokens[2:5])
-    if head1 != "matrix"
-        throw(ParseError("Unknown MatrixMarket data type: $head1 (only \"matrix\" is supported)"))
-    end
-
-    eltype = field == "real" ? Float64 :
-             field == "complex" ? Complex128 :
-             field == "integer" ? Int64 :
-             field == "pattern" ? Bool :
-             throw(ParseError("Unsupported field $field (only real and complex are supported)"))
-
-    symlabel = symm == "general" ? identity :
-               symm == "symmetric" ? symmetric! :
-               symm == "hermitian" ? hermitian! :
-               symm == "skew-symmetric" ? skewsymmetric! :
-               throw(ParseError("Unknown matrix symmetry: $symm (only general, symmetric, skew-symmetric and hermitian are supported)"))
-
-    # Skip all comments and empty lines
-    ll   = readline(mmfile)
-    while length(chomp(ll))==0 || (length(ll) > 0 && ll[1] == '%')
-        ll = readline(mmfile)
-    end
-    # Read matrix dimensions (and number of entries) from first non-comment line
-    dd = map(x->parse(Int, x), split(ll))
-    if length(dd) < (rep == "coordinate" ? 3 : 2)
-        throw(ParseError(string("Could not read in matrix dimensions from line: ", ll)))
-    end
-    rows = dd[1]
-    cols = dd[2]
-    entries = (rep == "coordinate") ? dd[3] : (rows * cols)
-    if infoonly
-        return (rows, cols, entries, rep, field, symm)
-    end
-    if rep == "coordinate"
-        rr = Array(Int, entries)
-        cc = Array(Int, entries)
-        xx = Array(eltype, entries)
-        for i in 1:entries
-            line = readline(mmfile)
-
-            num_splits = if eltype == Complex128
-                             3
-                         elseif eltype == Bool
-                             1
-                         else
-                             2
-                         end
-            splits = find_splits(line, num_splits)
-
-            rr[i] = parse(Int, line[1:splits[1]])
-            if eltype == Bool
-                cc[i] = parse(Int, line[splits[1]:end])
-            else
-                cc[i] = parse(Int, line[splits[1]:splits[2]])
-            end
-
-            if eltype == Complex128
-                real = parse(Float64, line[splits[2]:splits[3]])
-                imag = parse(Float64, line[splits[3]:length(line)])
-                xx[i] = Complex128(real, imag)
-            elseif eltype == Bool
-                xx[i] = true
-            else
-                xx[i] = parse(eltype, line[splits[2]:length(line)])
-            end
+    open(filename,"r") do mmfile
+        # Read first line
+        firstline = chomp(readline(mmfile))
+        tokens = split(firstline)
+        if length(tokens) != 5
+            throw(ParseError(string("Not enough words on first line: ", ll)))
         end
-        return symlabel(sparse(rr, cc, xx, rows, cols))
+        if tokens[1] != "%%MatrixMarket"
+            throw(ParseError(string("Not a valid MatrixMarket header:", ll)))
+        end
+        (head1, rep, field, symm) = map(lowercase, tokens[2:5])
+        if head1 != "matrix"
+            throw(ParseError("Unknown MatrixMarket data type: $head1 (only \"matrix\" is supported)"))
+        end
+
+        eltype = field == "real" ? Float64 :
+                 field == "complex" ? Complex128 :
+                 field == "integer" ? Int64 :
+                 field == "pattern" ? Bool :
+                 throw(ParseError("Unsupported field $field (only real and complex are supported)"))
+
+        symlabel = symm == "general" ? identity :
+                   symm == "symmetric" ? symmetric! :
+                   symm == "hermitian" ? hermitian! :
+                   symm == "skew-symmetric" ? skewsymmetric! :
+                   throw(ParseError("Unknown matrix symmetry: $symm (only general, symmetric, skew-symmetric and hermitian are supported)"))
+
+        # Skip all comments and empty lines
+        ll   = readline(mmfile)
+        while length(chomp(ll))==0 || (length(ll) > 0 && ll[1] == '%')
+            ll = readline(mmfile)
+        end
+        # Read matrix dimensions (and number of entries) from first non-comment line
+        dd = map(x->parse(Int, x), split(ll))
+        if length(dd) < (rep == "coordinate" ? 3 : 2)
+            throw(ParseError(string("Could not read in matrix dimensions from line: ", ll)))
+        end
+        rows = dd[1]
+        cols = dd[2]
+        entries = (rep == "coordinate") ? dd[3] : (rows * cols)
+        if infoonly
+            return (rows, cols, entries, rep, field, symm)
+        end
+        if rep == "coordinate"
+            rr = Array(Int, entries)
+            cc = Array(Int, entries)
+            xx = Array(eltype, entries)
+            for i in 1:entries
+                line = readline(mmfile)
+
+                num_splits = if eltype == Complex128
+                                 3
+                             elseif eltype == Bool
+                                 1
+                             else
+                                 2
+                             end
+                splits = find_splits(line, num_splits)
+
+                rr[i] = parse(Int, line[1:splits[1]])
+                if eltype == Bool
+                    cc[i] = parse(Int, line[splits[1]:end])
+                else
+                    cc[i] = parse(Int, line[splits[1]:splits[2]])
+                end
+
+                if eltype == Complex128
+                    real = parse(Float64, line[splits[2]:splits[3]])
+                    imag = parse(Float64, line[splits[3]:length(line)])
+                    xx[i] = Complex128(real, imag)
+                elseif eltype == Bool
+                    xx[i] = true
+                else
+                    xx[i] = parse(eltype, line[splits[2]:length(line)])
+                end
+            end
+            return symlabel(sparse(rr, cc, xx, rows, cols))
+        end
+        return symlabel(reshape([parse(Float64, readline(mmfile)) for i in 1:entries], (rows,cols)))
     end
-    return symlabel(reshape([parse(Float64, readline(mmfile)) for i in 1:entries], (rows,cols)))
 end
 
 function find_splits(s :: String, num)
