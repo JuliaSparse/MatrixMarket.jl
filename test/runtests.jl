e03b6468cff9398aec8ce2f72af7bd122465f614
Tristan Konolige <tristan.konolige@gmail.com>
2019-07-22 16:11:42 -0700

    Improve formatting of tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,16 +8,18 @@ include("dl-matrixmarket.jl")
 num_errors = 0
 num_pass = 0
 
-@testset "read and write $filename" for filename in filter(t -> endswith(t, ".mtx"), readdir())
-    new_filename = "$(filename)_"
-    A = MatrixMarket.mmread(filename)
-    Compat.@info("$(typeof(A))  $(size(A))")
+@testset "Idempotent IO" begin
+    @testset "read and write $filename" for filename in filter(t -> endswith(t, ".mtx"), readdir())
+        new_filename = "$(filename)_"
+        A = MatrixMarket.mmread(filename)
+        Compat.@info("$(typeof(A))  $(size(A))")
 
-    # verify mmread(mmwrite(A)) == A
-    MatrixMarket.mmwrite(new_filename, A)
-    new_A = MatrixMarket.mmread(new_filename)
-    @test new_A == A
+        # verify mmread(mmwrite(A)) == A
+        MatrixMarket.mmwrite(new_filename, A)
+        new_A = MatrixMarket.mmread(new_filename)
+        @test new_A == A
 
-    rm(filename)
-    rm(new_filename)
+        rm(filename)
+        rm(new_filename)
+    end
 end
